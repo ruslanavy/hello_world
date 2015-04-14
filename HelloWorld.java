e84af56c2b19c2f42587f5dceb97627eba1650e4
Ruslana <mynamestrawberries@gmail.com>
2015-04-14 20:31:35 +0300

    Add unuseful integer

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int i = 4;
 		System.out.println("Hello world!!!");
 		System.out.println("Hi there!");
 	}
